4fdee45aead6d6a61b91d0d755e424b6a3c99010
haitam nefal <haitam.nefal@gmail.com>
2026-09-01 14:32:58 +0200

    fixe le probleme de numero apre la virgule

--- Day1/variables/challenge2.js
+++ Day1/variables/challenge2.js
@@ -3,6 +3,9 @@ const prompt = require("prompt-sync")()
 const montantMAD = prompt("Montant Dirham : ")
 const montantEUR = montantMAD / 11
 
+const converToString = `${montantEUR}`
+const indexOfPoint = converToString.indexOf(".")
+
 
 console.log("Budget en MAD : ", montantMAD)
-console.log("Budget en EUR : ", montantEUR)
+console.log("Budget en EUR : ", converToString.slice(0, indexOfPoint+3))
