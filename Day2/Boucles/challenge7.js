17ff8f60c839e6930d0db8b0cb364fbd228213a5
haitam nefal <haitam.nefal@gmail.com>
2026-09-02 12:25:42 +0200

    complete challenge 7 : Trouver le premier nombre supérieur

---
+++ Day2/Boucles/challenge7.js
@@ -0,0 +1,11 @@
+
+let result = 1
+
+while (true) {
+    let value = result * result
+    if (value > 100) break
+    result++
+}
+
+console.log(`Result : ${result}`)
+console.log(`${result} x ${result} = ${result * result}`)
