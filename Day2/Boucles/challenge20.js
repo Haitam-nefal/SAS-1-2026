17e86a8ba1a7eb72be977117dc0b0566ab3943b9
haitam nefal <haitam.nefal@gmail.com>
2026-09-03 11:10:21 +0200

    complete challenge 20 :Palindrome numérique

---
+++ Day2/Boucles/challenge20.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")()
+
+
+let nbr = Number(prompt("Nombre : "))
+
+let nbr2 = nbr
+let palindromNbr = 0
+
+while (nbr != 0) {
+    const a = nbr % 10
+    palindromNbr = palindromNbr * 10 + a
+    nbr = parseInt(nbr / 10)
+}
+
+// console.log(palindromNbr);
+// console.log(nbr2);
+
+console.log(palindromNbr === nbr2 ? `${nbr2} → palindrome` : `${nbr2} → pas palindrome`);
