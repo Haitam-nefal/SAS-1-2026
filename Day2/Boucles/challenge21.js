8b70f66b0188d523293c814964c8c0ce8d09ea9f
haitam nefal <haitam.nefal@gmail.com>
2026-09-03 11:44:26 +0200

    ajouter la somme de tous les nombres promiers

--- Day2/Boucles/challenge21.js
+++ Day2/Boucles/challenge21.js
@@ -2,11 +2,17 @@ const prompt = require("prompt-sync")()
 
 const debut = prompt("Debt : ")
 const fin = prompt("Fin : ")
+let somme = 0
 
 for (let i = debut; i <= fin; i++) {
     let k = 0
     for (let j = 2; j <= (i / 2); j++) {
         if (i % j == 0) k++
     }
-    if (!k) console.log(i); // k = 0
+    if (!k) {
+        console.log(i)
+        somme += i
+    }
 }
+
+console.log(`Somme : ${somme}`);
